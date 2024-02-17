6f3647ff7dc846591ccd1ff0f738250a9244bef4
abjaryal <jaryal.jerry@gmail.com>
2024-02-17 21:07:13 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UiComponentsModule } from 'projects/ui-components/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
